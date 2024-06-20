ec5c23a4ef9039f42180c9379f844ee61e2b5412
Arafath Qureshi <arfathq9@gmail.com>
2024-06-20 18:16:13 +0530

    Fix Vite configuration for Render deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
-      "/api": {
-        target: "auto", // Automatically proxy to same host as Vite dev server or production server
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
+      "/api": "http://localhost:3000",
     },
   },
   plugins: [react()],
